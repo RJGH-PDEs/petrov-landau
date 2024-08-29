07ae04e3e6672a6fc921ed999a3e8095ad2970d2
Rodrigo Gonzalez <rodrigogonzalez@utexas.edu>
2024-08-29 17:11:58 -0500

    derivatives of Legendre polynomials

--- src/basis_grad.py
+++ src/basis_grad.py
@@ -1,7 +1,7 @@
 # Import
 from weight import *
 from scipy.special import genlaguerre
-
+from scipy.special import lpmv
 
 # Play with Laguerre polynomial
 def laguerre_example():
@@ -11,6 +11,19 @@ def laguerre_example():
 
     # Evaluate an associated Laguerre
     print(genlaguerre(n,alpha)(x)) # <-- need to check if it is computing right
+
+# Play with Legendre polynomial 
+def legendre_example():
+    """
+    Play with legendre polynomial
+    """
+    # Parameters for the polynomial
+    m = 0
+    l = 2
+    x = 0
+
+    # Evaluate a Laguerre polynomial
+    print(lpmv(m,l,x))
 
 # Phi(r): the radial part of the test functions
 def Phi(l, k, r):
@@ -52,14 +65,57 @@ def Phi_r(l, k, r):
     # return
     return result 
 
+# Derivative of a Leguerre polynomial
+def Leg_theta(m, l, t):
+    """
+    Computes the partial derivative of Leg_{m,l}(cos theta)
+    with respect of theta. Used for computing the partial
+    derivative of a spherical harmonic. Recursive formula
+    not valid for theta equal to multiples of pi
+
+    Parameters:
+        - m:    int
+        - l:    int
+        - t:    float, the angle theta
+    """
+    result = 0
+
+    # handle the constant case
+    if(m == 0 and l == 0):
+        return 0
+
+    # Conversion from theta to x
+    x = np.cos(t)
+
+    # First part
+    result = (-l-1)*lpmv(m, l, x)*x
+
+    # Second part
+    result = result + (l + 1 - m)*lpmv(m, l + 1, x)
+
+    # Denumenator
+    result = result/np.sin(t)
+
+    return result
+
 # The main function
 def main():
     l = 3
     k = 2
     r = 1
     # Compute the derivative 
-    print('Partial derivative:', Phi_r(l,k,r))
+    # print('Partial derivative:', Phi_r(l,k,r))
 
+    # Legendre function
+    # legendre_example()
+
+    # Partial derivative of the associated legendre
+    m       = 2
+    l       = 2
+    theta   = np.pi/4
+
+    # Compute the derivative 
+    print('Partial derivative: ', Leg_theta(m,l,theta))
 
 # Main function
 if __name__ == "__main__":
