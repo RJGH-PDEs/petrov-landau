6f90d1c23546b837407ea6489d56c0556e14e6a5
Rodrigo Gonzalez <rodrigogonzalez@utexas.edu>
2024-08-30 18:29:02 -0500

    Partials of test functions done, need to test

--- src/basis_partials.py
+++ src/basis_partials.py
@@ -1,4 +1,5 @@
 from basis_grad import *
+from basis import *
 
 # w.r.t r
 def partial_r(k, l, m, r, t, p):
@@ -28,6 +29,41 @@ def partial_theta(k, l, m, r, t ,p):
     result = 0
 
     # Radial part
-    result = Phi_r(l, k, r)
+    result = Phi_over_r(l, k, r)
+
+    # theta part 
+    result = result * Leg_t(m, l, t)
+
+    # phi part
+    result = result * azimuth(m, p)
+
+    # Constant
+    result = result*spher_const(l, m)
+
+    return result
+
+# w.r.t. phi
+def partial_phi(k, l, m, r, t ,p):
+    """
+    This function will compute (1/r sin(theta)) f_(phi) where
+    f is a basis function. Note that this takes 
+    care of the division by r automatically
+    """
+    result = 0
+
+    # Radial part
+    result = Phi_over_r(l, k, r)
+
+    # theta part 
+    result = result * Leg(m, l, t)
+
+    # phi part
+    result = result * azim_p(m, p)
+
+    # Constant
+    result = spher_const(l, m)
+
+    # divide by sin(theta)
+    result = result/np.sin(t)
 
     return result
