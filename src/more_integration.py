543c086f6b5ad54fe27e32b6f1816649b526bfd6
Rodrigo Gonzalez <rodrigogonzalez@rodrigos-mbp.lan>
2024-09-10 21:07:20 -0500

    radial integration

--- src/more_integration.py
+++ src/more_integration.py
@@ -1,9 +1,10 @@
 from scipy.special import roots_genlaguerre
+from scipy.special import genlaguerre
 
 # play with Laguerre integration
 def genLaguerreInt():
     # extract the coefficients
-    n = 3
+    n = 4
     alpha = 1/2
     x,w = roots_genlaguerre(n, alpha, False)
 
@@ -21,9 +22,43 @@ def genLaguerreInt():
     print("Weights: ")
     print(w[0])
 
+# radial function to be integrated
+def integrand(s):
+    # parameters
+    l = 1
+    alpha = l + 1/2
+    k = 0
+
+    # compute the result
+    result = 0
+    x = 2*s
+    result = genlaguerre(k, alpha)(x)
+    result = result*(s**(l/2))
+    result = result*(2**((l+1)/2))
+    
+    # return 
+    # print(result)
+    return result     
+
+# 
+def integrating_integrand():
+    # extract the coefficients
+    n = 6
+    alpha = 1/2
+    x,w = roots_genlaguerre(n, alpha, False)
+
+    sum = 0
+    i = 0
+    for node in x:
+        print(node)
+        sum = sum + w[i]*integrand(node)
+        i = i + 1
+
+    print("integration result: ", sum)
+
 # The main function
 def main():
-    genLaguerreInt()
+    integrating_integrand()
 
 if __name__ == "__main__":
     main()
